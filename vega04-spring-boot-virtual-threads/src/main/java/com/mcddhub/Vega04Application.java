bc3ab1a45903a7dead6a59c23b5ebd7894793cbb
caobaoqi1029 <2024cbq@gmail.com>
2024-09-24 21:53:07 +0800

    :sparkles: feat(vega04): init file

---
+++ vega04-spring-boot-virtual-threads/src/main/java/com/mcddhub/Vega04Application.java
@@ -0,0 +1,11 @@
+package com.mcddhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vega04Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Vega04Application.class, args);
+    }
+}
