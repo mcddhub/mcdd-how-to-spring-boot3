861f6d6372e7734b79c64deda418f752a3e25074
caobaoqi1029 <2024cbq@gmail.com>
2024-09-24 21:07:45 +0800

    :sparkles: feat(vega03): init file

---
+++ vega-03-spring-boot-rest-client-test/src/main/java/com/mcddhub/Vega03Application.java
@@ -0,0 +1,11 @@
+package com.mcddhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vega03Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Vega03Application.class, args);
+    }
+}
